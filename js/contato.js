dc920a620bd2d735b994e734ba6078201bbd254c
Nezio22 <matheus.nezio22@gmail.com>
2026-05-24 18:28:48 -0300

    feat(contato): validação e envio do formulário de contato

---
+++ js/contato.js
@@ -0,0 +1,32 @@
+function enviarFormulario(){
+
+    let nome =
+    document.getElementById("nome").value;
+
+    let email =
+    document.getElementById("email").value;
+
+    let mensagem =
+    document.getElementById("mensagem").value;
+
+    if(
+        nome == "" ||
+        email == "" ||
+        mensagem == ""
+    ){
+
+        alert(
+            "Preencha todos os campos!"
+        );
+
+        return;
+    }
+
+    alert(
+        "Mensagem enviada com sucesso!"
+    );
+
+    document
+    .getElementById("formContato")
+    .reset();
+}
